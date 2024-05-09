8d6880be82f7d59bf7f9006da8379364caaed054
Antonio <antonio.liubchev@gmail.com>
2024-05-09 11:50:51 +0300

    fix path in actions #18

--- tls/create_default_keystore.bat
+++ tls/create_default_keystore.bat
@@ -9,17 +9,12 @@ if not exist "%KEYSTORE_DIR%" (
     mkdir "%KEYSTORE_DIR%"
 )
 
-:: Set default values
 set KEYSTORE_NAME=springboot.p12
 set ALIAS=api
 set VALIDITY=3650
 set KEY_SIZE=2048
+set KEYPASS=123456
 
-:: Prompt only for the mandatory keystore password
-echo Enter the keystore password (minimum 6 characters):
-set /p KEYPASS=
-
-:: Create the keystore in the specified folder
 echo Creating the keystore...
 keytool -genkeypair -alias %ALIAS% -keyalg RSA -keysize %KEY_SIZE% -storetype PKCS12 -keystore "%KEYSTORE_DIR%\%KEYSTORE_NAME%" -validity %VALIDITY% -storepass %KEYPASS% -keypass %KEYPASS% -dname "CN=Unknown, OU=Unknown, O=Unknown, L=Unknown, S=Unknown, C=Unknown" >nul 2>&1
 if %errorlevel% neq 0 (
